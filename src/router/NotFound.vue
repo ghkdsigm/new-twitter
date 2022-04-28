938e3a30e4eeaef1ab76a0a5d45af186fdc5f66b
2022project <ghkdsigm3@gmail.com>
2022-04-29 04:38:56 +0900

    404페이지 텍스트 수정

--- src/router/NotFound.vue
+++ src/router/NotFound.vue
@@ -6,7 +6,7 @@
     <div class="text-9xl">404</div>
     <div class="text-2xl">페이지가 존재하지않습니다.</div>
     <button>
-      <a href="/" class="border-2 border-black hover:text-primary hover:border-primary dark:border-gray-100 dark:hover:border-primary py-2 px-6 mt-5 block rounded-full">메인으로</a>
+      <a href="/" class="border-2 border-black hover:text-primary hover:border-primary dark:border-gray-100 dark:hover:border-primary py-2 px-6 mt-5 block rounded-full">홈으로</a>
     </button>
   </div>
 </template>
